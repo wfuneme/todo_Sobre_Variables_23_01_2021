bae20f4f8ef431eed60a43b873a6100372b55385
Willmer Obed Fúneme San Martín <willmer.funeme@unac.edu.co>
2021-01-23 21:51:24 -0500

    Taller Conceptos básicos JavaScript - punto 4

---
+++ main.js
@@ -0,0 +1,22 @@
+// Variable tipo string
+alert ("👉Ver la consola del navegador");
+
+var string = "Variable tipo String 'Hola' 🆎";
+console.log(string);
+
+// Variable tipo number
+
+var number = "Variable tipo Number 12354 📆";
+console.log(number);
+
+// Variable tipo Boolean
+
+var boolean_soy_studiante = "Variable tipo Boolean ✅ 'true'";;
+console.log(boolean_soy_studiante);
+
+// variable tipo Constante (declaración)
+
+const cedula = 123456789;
+
+console.warn(cedula);
+console.log(cedula);
